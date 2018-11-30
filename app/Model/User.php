6ef570dfd1e882478c78d85adcce3c650e203717
Lemon <534076424@qq.com>
2018-11-30 10:32:18 +0800

    generate ide-laravel-helper file

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,6 +6,31 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\Model\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
